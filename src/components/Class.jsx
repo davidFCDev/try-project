e9b2d275de6b4358148daf31cb2bcbf660745d3f
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-17 16:57:14 +0200

    animaciones

--- src/components/Class.jsx
+++ src/components/Class.jsx
@@ -1,14 +1,30 @@
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
 const Class = () => {
+  const animationOptions = {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1.2 },
+  };
+
+  const [ref, inView] = useInView({ threshold: 0.6, triggerOnce: true });
+
   return (
     <div className="w-full py-32 flex flex-col gap-32">
-      <div className="flex flex-col gap-4 w-[40%] px-20 items-start">
+      <motion.div
+        animate={inView ? animationOptions : {}}
+        initial={{ opacity: 0, y: 50 }}
+        className="flex flex-col gap-4 w-[40%] px-20 items-start"
+        ref={ref}
+      >
         <h2 className="font-creatoExtrBold text-6xl uppercase tracking-wider">
           Nuestras clases
         </h2>
         <p className="font-creatoLight text-2xl">
-          Amplia variedad de clases que podrás disfrutar en nuestros centros. Descubre la que más se adapta a ti.
+          Amplia variedad de clases que podrás disfrutar en nuestros centros.
+          Descubre la que más se adapta a ti.
         </p>
         <Link
           to={"/contact"}
@@ -16,17 +32,19 @@ const Class = () => {
         >
           Ver horarios
         </Link>
-      </div>
+      </motion.div>
 
       <div className="flex flex-col gap-10 w-full justify-around">
         <div className="flex gap-4">
-          <img
-            src="https://media.istockphoto.com/id/1263905214/es/foto/espacio-de-gimnasio.jpg?s=2048x2048&w=is&k=20&c=fsMWkvT_afyr4iL7H0FMcNF-ldbbe9-D8_yPkbdC0do="
-            alt="icono wod"
-            className="w-full h-[200px] object-cover object-center"
-          />
+          <div className="w-full overflow-hidden">
+            <img
+              src="https://media.istockphoto.com/id/1263905214/es/foto/espacio-de-gimnasio.jpg?s=2048x2048&w=is&k=20&c=fsMWkvT_afyr4iL7H0FMcNF-ldbbe9-D8_yPkbdC0do="
+              alt="icono wod"
+              className="w-full h-[200px] object-cover object-center transform hover:scale-110 transition-all duration-1000"
+            />
+          </div>
           <div className="flex flex-col items-start justify-center gap-5 px-5">
-            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wide">
+            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wider">
               Wod
             </h3>
             <p className="text-xl font-creato">
@@ -39,13 +57,15 @@ const Class = () => {
         </div>
 
         <div className="flex flex-row-reverse gap-4">
-          <img
-            src="https://media.istockphoto.com/id/1001575694/es/foto/hombre-joven-de-entrenamiento-fitness-flexiones-o-tabl%C3%B3n.jpg?s=2048x2048&w=is&k=20&c=LRCb3eohMi_daMHdJbJ11SBL73GN46ZVJ1PAd8dnqu0="
-            alt="icono wod"
-            className="w-full h-[200px] object-cover object-center"
-          />
+          <div className="w-full overflow-hidden">
+            <img
+              src="https://media.istockphoto.com/id/1001575694/es/foto/hombre-joven-de-entrenamiento-fitness-flexiones-o-tabl%C3%B3n.jpg?s=2048x2048&w=is&k=20&c=LRCb3eohMi_daMHdJbJ11SBL73GN46ZVJ1PAd8dnqu0="
+              alt="icono wod"
+              className="w-full h-[200px] object-cover object-center transform hover:scale-110 transition-all duration-1000"
+            />
+          </div>
           <div className="flex flex-col items-end text-right justify-center gap-5 px-5">
-            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wide">
+            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wider">
               Endurance
             </h3>
             <p className="text-xl font-creato">
@@ -58,13 +78,15 @@ const Class = () => {
         </div>
 
         <div className="flex gap-4">
-          <img
-            src="https://media.istockphoto.com/id/1166399307/es/foto/entrenamiento-cruzado-con-campanas-de-hervidor-imagen-de-bajo-%C3%A1ngulo-espacio-de-copia.jpg?s=2048x2048&w=is&k=20&c=xSVGAlj_hr5Y8NOJ9VzExz8vNXm_wMmVuhTONIpxiEw="
-            alt="icono wod"
-            className="w-full h-[200px] object-cover object-center"
-          />
+          <div className="w-full overflow-hidden">
+            <img
+              src="https://media.istockphoto.com/id/1166399307/es/foto/entrenamiento-cruzado-con-campanas-de-hervidor-imagen-de-bajo-%C3%A1ngulo-espacio-de-copia.jpg?s=2048x2048&w=is&k=20&c=xSVGAlj_hr5Y8NOJ9VzExz8vNXm_wMmVuhTONIpxiEw="
+              alt="icono wod"
+              className="w-full h-[200px] object-cover object-center transform hover:scale-110 transition-all duration-1000"
+            />
+          </div>
           <div className="flex flex-col items-start justify-center gap-5 px-5">
-            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wide">
+            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wider">
               TRY45
             </h3>
             <p className="text-xl font-creato">
@@ -77,13 +99,15 @@ const Class = () => {
         </div>
 
         <div className="flex flex-row-reverse gap-4">
-          <img
-            src="https://media.istockphoto.com/id/494498164/es/foto/hermosa-bailarina-en-vista-a%C3%A9rea-de-seda-a%C3%A9rea-a%C3%A9rea-contortion-cintas.jpg?s=2048x2048&w=is&k=20&c=JCW0GkgCq5_lcmG9MnsFNdHU3_4pWvL_AHXhXuPsSeg="
-            alt="icono wod"
-            className="w-full h-[200px] object-cover object-center"
-          />
+          <div className="w-full overflow-hidden">
+            <img
+              src="https://media.istockphoto.com/id/494498164/es/foto/hermosa-bailarina-en-vista-a%C3%A9rea-de-seda-a%C3%A9rea-a%C3%A9rea-contortion-cintas.jpg?s=2048x2048&w=is&k=20&c=JCW0GkgCq5_lcmG9MnsFNdHU3_4pWvL_AHXhXuPsSeg="
+              alt="icono wod"
+              className="w-full h-[200px] object-cover object-center transform hover:scale-110 transition-all duration-1000"
+            />
+          </div>
           <div className="flex flex-col items-end text-right justify-center gap-5 px-5">
-            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wide">
+            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wider">
               AERIAL
             </h3>
             <p className="text-xl font-creato">
@@ -96,13 +120,15 @@ const Class = () => {
         </div>
 
         <div className="flex gap-4">
-          <img
-            src="https://media.istockphoto.com/id/1222084311/es/foto/imagen-de-barbell-en-el-gimnasio.jpg?s=2048x2048&w=is&k=20&c=kXHLejjkmU0ePIh_FU_Benz2VMke5bCe_Nn8PwOpzHQ="
-            alt="icono wod"
-            className="w-full h-[200px] object-cover object-bottom"
-          />
+          <div className="w-full overflow-hidden">
+            <img
+              src="https://media.istockphoto.com/id/1222084311/es/foto/imagen-de-barbell-en-el-gimnasio.jpg?s=2048x2048&w=is&k=20&c=kXHLejjkmU0ePIh_FU_Benz2VMke5bCe_Nn8PwOpzHQ="
+              alt="icono wod"
+              className="w-full h-[200px] object-cover object-bottom transform hover:scale-110 transition-all duration-1000"
+            />
+          </div>
           <div className="flex flex-col items-start justify-center gap-5 px-5">
-            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wide">
+            <h3 className="text-4xl uppercase font-creatoExtrBold tracking-wider">
               OPEN BOX
             </h3>
             <p className="text-xl font-creato">
