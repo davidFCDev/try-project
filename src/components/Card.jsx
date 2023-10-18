91b77b8c06f34eaddebcdd02205b81619bcc8cf8
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-18 23:55:25 +0200

    mas estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,14 +5,27 @@ const Card = ({ price }) => {
     <div className="card">
       <div className="header">
         <span className="title font-creatoBold">{price.price}</span>
-        <span className="price font-creatoExtrBold">{price.title}</span>
+        <span className="price font-creatoExtrBold tracking-wider">{price.title}</span>
       </div>
-      <p className="desc font-creatoBold text-red-600 text-xl tracking-wide">{price.center}</p>
-      <ul className="lists font-creatoLight text-lg">
+      <p className="desc font-creatoBold text-red-600 text-xl tracking-wide">
+        {price.center}
+      </p>
+      <ul className="lists font-creatoLight text-base">
         <li className="list">
-          <span className="font-creato text-lg">{price.description[0]}</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <span>{price.description[0]}</span>
         </li>
-        <li className="list text-sm">
+        <li className="list text-base">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -26,7 +39,7 @@ const Card = ({ price }) => {
           </svg>
           <span>{price.description[1]}</span>
         </li>
-        <li className="list text-sm">
+        <li className="list text-base">
           {price.description[2] ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
