ffbec46d4cee7ddc7aaffd78e21f6750614f38f7
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-22 22:20:49 +0200

    class item

--- src/constants/class.js
+++ src/constants/class.js
@@ -8,6 +8,8 @@ export const CLASS = [
       end: "OD",
     },
     subtitle: "Workout of the day, nuestra clase más exigente",
+    description:
+      "Est ipsum ea officia amet occaecat sint tempor. Qui et duis ad ex labore qui occaecat deserunt sunt magna aliqua cillum. Adipisicing nisi irure enim consectetur et. Id ut laborum amet esse id amet ad aliqua sunt in ad.",
     image:
       "https://media.istockphoto.com/id/1263905214/es/foto/espacio-de-gimnasio.jpg?s=2048x2048&w=is&k=20&c=fsMWkvT_afyr4iL7H0FMcNF-ldbbe9-D8_yPkbdC0do=",
   },
@@ -21,6 +23,8 @@ export const CLASS = [
       end: "RANCE",
     },
     subtitle: "Si te gusta quemar calorías, esta es tu clase",
+    description:
+      "Est ipsum ea officia amet occaecat sint tempor. Qui et duis ad ex labore qui occaecat deserunt sunt magna aliqua cillum. Adipisicing nisi irure enim consectetur et. Id ut laborum amet esse id amet ad aliqua sunt in ad.",
     image:
       "https://media.istockphoto.com/id/1001575694/es/foto/hombre-joven-de-entrenamiento-fitness-flexiones-o-tabl%C3%B3n.jpg?s=2048x2048&w=is&k=20&c=LRCb3eohMi_daMHdJbJ11SBL73GN46ZVJ1PAd8dnqu0=",
   },
@@ -34,6 +38,8 @@ export const CLASS = [
       red: "5",
     },
     subtitle: "Entrenamientos específicos de 45 minutos",
+    description:
+      "Est ipsum ea officia amet occaecat sint tempor. Qui et duis ad ex labore qui occaecat deserunt sunt magna aliqua cillum. Adipisicing nisi irure enim consectetur et. Id ut laborum amet esse id amet ad aliqua sunt in ad.",
     image:
       "https://media.istockphoto.com/id/1166399307/es/foto/entrenamiento-cruzado-con-campanas-de-hervidor-imagen-de-bajo-%C3%A1ngulo-espacio-de-copia.jpg?s=2048x2048&w=is&k=20&c=xSVGAlj_hr5Y8NOJ9VzExz8vNXm_wMmVuhTONIpxiEw=",
   },
@@ -47,6 +53,8 @@ export const CLASS = [
       end: "RIAL",
     },
     subtitle: "Especialidad aérea, para los más atrevidos",
+    description:
+      "Est ipsum ea officia amet occaecat sint tempor. Qui et duis ad ex labore qui occaecat deserunt sunt magna aliqua cillum. Adipisicing nisi irure enim consectetur et. Id ut laborum amet esse id amet ad aliqua sunt in ad.",
     image:
       "https://media.istockphoto.com/id/494498164/es/foto/hermosa-bailarina-en-vista-a%C3%A9rea-de-seda-a%C3%A9rea-a%C3%A9rea-contortion-cintas.jpg?s=2048x2048&w=is&k=20&c=JCW0GkgCq5_lcmG9MnsFNdHU3_4pWvL_AHXhXuPsSeg=",
   },
@@ -59,6 +67,8 @@ export const CLASS = [
       end: "PEN BOX",
     },
     subtitle: "Clase de prueba de 45 minutos",
+    description:
+      "Est ipsum ea officia amet occaecat sint tempor. Qui et duis ad ex labore qui occaecat deserunt sunt magna aliqua cillum. Adipisicing nisi irure enim consectetur et. Id ut laborum amet esse id amet ad aliqua sunt in ad.",
     image:
       "https://media.istockphoto.com/id/1325226607/es/foto/caja-de-saltos-y-otros-equipos-en-el-gimnasio-vac%C3%ADo.jpg?s=2048x2048&w=is&k=20&c=QXwzzxwYWRB9m0O2T4TkDgOkMRKU_A5rj75Sc8rTF88=",
   },
--- src/pages/ClassPage.jsx
+++ src/pages/ClassPage.jsx
@@ -1,19 +1,38 @@
-// import { useParams } from "react-router-dom";
-// import { CLASS } from "../constants/class.js";
+import { useParams } from "react-router-dom";
+import { CLASS } from "../constants/class.js";
 
 const ClassPage = () => {
-  // const { classId } = useParams();
-  // const selectedClass = CLASS.find((classItem) => classItem.id === parseInt(classId, 10));
+  const { id } = useParams();
+  const selectedClass = CLASS.find(
+    (classItem) => classItem.id === parseInt(id, 10)
+  );
 
-  // if (!selectedClass) {
-  //   return <div>Clase no encontrada</div>;
-  // }
+  if (!selectedClass) {
+    return <div>Clase no encontrada</div>;
+  }
+
+  console.log(selectedClass);
 
   return (
-    <div className="w-full min-h-screen py-28 px-10 gap-10 justify-start flex flex-col items-center bg-dark">
-      <h2 className="font-creatoBold text-5xl uppercase tracking-wider ">
-        Contacto
-      </h2>
+    <div className="w-full min-h-screen py-32 gap-10 justify-end flex items-center bg-dark text-white">
+      <div className="flex flex-col gap-10 w-[40%]">
+        <h2 className="font-creatoBold text-7xl uppercase tracking-wider ">
+          {selectedClass.title.first}
+          {selectedClass.title.red}
+          {selectedClass.title.end}
+        </h2>
+        <div className="flex flex-col gap-5">
+          <h3 className="font-creatoBold text-4xl">{selectedClass.subtitle}</h3>
+          <p className="font font-creatoLight text-xl">
+            {selectedClass.description}
+          </p>
+        </div>
+      </div>
+      <img
+        src={selectedClass.image}
+        alt="image from class"
+        className="w-[50%] rounded-md"
+      />
     </div>
   );
 };
