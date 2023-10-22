c73e715ebd1dcf3f49a54a831805959cb0d4b22e
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-22 21:35:17 +0200

    contacto

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,17 +5,17 @@ const ContactPage = () => {
         Contacto
       </h2>
 
-      <form className="w-full max-w-lg shadow-sm shadow-white p-10 bg-white rounded-sm">
+      <form className="w-full max-w-lg shadow-sm shadow-white p-10 bg-gray-100 rounded-md">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
             <label
-              className="block uppercase text-md font-bold mb-2 font-creatoExtrBold tracking-widest"
+              className="block uppercase text-md font-bold mb-2 font-creatoExtrBold tracking-wider"
               htmlFor="grid-first-name"
             >
               Nombre
             </label>
             <input
-              className="appearance-none block w-full py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 bg-gray-100"
+              className="appearance-none block w-full py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 bg-white rounded-sm"
               id="grid-first-name"
               type="text"
               placeholder="Nombre"
@@ -26,13 +26,13 @@ const ContactPage = () => {
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
             <label
-              className="block uppercase text-md font-bold mb-2 font-creatoExtrBold tracking-widest"
+              className="block uppercase text-md font-bold mb-2 font-creatoBold tracking-wider"
               htmlFor="grid-password"
             >
               E-mail
             </label>
             <input
-              className="appearance-none block w-full py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 bg-gray-100"
+              className="appearance-none block w-full py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 bg-white rounded-sm"
               id="grid-password"
               type="email"
               placeholder="E-mail"
@@ -42,21 +42,21 @@ const ContactPage = () => {
         <div className="flex flex-wrap -mx-3 mb-2">
           <div className="w-full px-3">
             <label
-              className="block uppercase text-md font-bold mb-2 font-creatoExtrBold tracking-widest"
+              className="block uppercase text-md font-bold mb-2 font-creatoBold tracking-wider"
               htmlFor="grid-password"
             >
               Mensaje
             </label>
             <textarea
-              className="appearance-none block w-full py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 bg-gray-100"
+              className="appearance-none block w-full py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-gray-200 bg-white rounded-sm"
               id="grid-password"
               type="text"
-              placeholder="Mensaje"
+              placeholder="Cuéntanos tus dudas"
             />
 
             <button
               type="submit"
-              className="bg-black font-creatoBold text-xl w-full font-bold py-2 px-4 text-white hover:bg-dark transition-all "
+              className="bg-black font-creatoBold text-xl w-full font-bold py-2 px-4 text-white hover:bg-gray-600 focus:bg-gray-600 transition-all "
             >
               Enviar
             </button>
