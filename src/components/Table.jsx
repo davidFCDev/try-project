dfac6f53b559d8dfad9eb26f493a79573fa88acf
David Fernández <davidfernandezcomesana@gmail.com>
2023-10-24 16:48:35 +0200

    horarios

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -27,7 +27,7 @@ const Table = () => {
       {loading ? (
         <p className="text-white text-xl">Cargando horarios...</p>
       ) : error ? (
-        <p>{error}</p>
+        <p className="text-white">{error}</p>
       ) : (
         <div className="bg-gray-200 w-full shadow-md shadow-gray-600">
           <table cellPadding={12} className="w-full border-separate">
@@ -45,13 +45,13 @@ const Table = () => {
             <tbody className="text-center">
               {timetable.map((item, index) => (
                 <tr key={index} className="font-creatoBold">
-                  <td className="border border-slate-400">{item[1]}</td>
-                  <td className="border border-slate-400">{item[2]}</td>
-                  <td className="border border-slate-400">{item[3]}</td>
-                  <td className="border border-slate-400">{item[4]}</td>
-                  <td className="border border-slate-400">{item[5]}</td>
-                  <td className="border border-slate-400">{item[6]}</td>
-                  <td className="border border-slate-400">{item[7]}</td>
+                  <td className={`border border-slate-400 ${item[1] === 'WOD' || item[1] === 'ENDURANCE' ? 'bg-red-200' : item[1] === 'TRY45' || item[1] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[1]}</td>
+                  <td className={`border border-slate-400 ${item[2] === 'WOD' || item[2] === 'ENDURANCE' ? 'bg-red-200' : item[2] === 'TRY45' || item[2] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[2]}</td>
+                  <td className={`border border-slate-400 ${item[3] === 'WOD' || item[3] === 'ENDURANCE' ? 'bg-red-200' : item[3] === 'TRY45' || item[3] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[3]}</td>
+                  <td className={`border border-slate-400 ${item[4] === 'WOD' || item[4] === 'ENDURANCE' ? 'bg-red-200' : item[4] === 'TRY45' || item[4] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[4]}</td>
+                  <td className={`border border-slate-400 ${item[5] === 'WOD' || item[5] === 'ENDURANCE' ? 'bg-red-200' : item[5] === 'TRY45' || item[5] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[5]}</td>
+                  <td className={`border border-slate-400 ${item[6] === 'WOD' || item[6] === 'ENDURANCE' ? 'bg-red-200' : item[6] === 'TRY45' || item[6] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[6]}</td>
+                  <td className={`border border-slate-400 ${item[7] === 'WOD' || item[7] === 'ENDURANCE' ? 'bg-red-200' : item[7] === 'TRY45' || item[7] === 'AERIAL' ? 'bg-blue-100' : ''}`}>{item[7]}</td>
                 </tr>
               ))}
             </tbody>
--- src/pages/TimetablePage.jsx
+++ src/pages/TimetablePage.jsx
@@ -3,9 +3,21 @@ import Table from "../components/Table";
 const TimetablePage = () => {
   return (
     <div className="w-full min-h-screen py-28 px-10 gap-10 justify-start flex flex-col items-center bg-dark">
-      <h2 className="font-creatoExtrBold text-5xl uppercase tracking-wider text-white">
-        Nuestros horarios
-      </h2>
+      <div className="flex flex-col gap-4">
+        <h2 className="font-creatoExtrBold text-5xl uppercase tracking-wider text-white">
+          Nuestros horarios
+        </h2>
+        <div className="text-white flex w-full justify-between px-2 font-creatoLight tracking-wide">
+          <div className="flex gap-2 items-center">
+            <div className="w-10 bg-red-200 p-2" />
+            <p>Try Fitness Box</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="w-10 bg-blue-200 p-2" />
+            <p>Try Fitness Zenter</p>
+          </div>
+        </div>
+      </div>
       <Table />
     </div>
   );
